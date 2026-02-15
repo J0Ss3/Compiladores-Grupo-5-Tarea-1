4952858ba18aea1caf1c79a8dbd16221fb4329d7
Michael Archaga <38266362+archaga19@users.noreply.github.com>
2026-02-15 11:46:14 -0600

    Update EJERCICIO_C.py

--- EJERCICIO_C.py
+++ EJERCICIO_C.py
@@ -2,6 +2,34 @@
 c.Extraer los nombres de archivos de un tipo de formato en una ruta de
 directorio, por ejemplo: C:\Documentos, archivos txt.
 """
+import re
+import os
 
 def c():
-    print('c')
+    print('--- Ejecutando Inciso C: Extracción de archivos .txt ---')
+    
+    ruta = input("Introducir la ruta de la carpeta (ejemplo C:\\Users\\Desktop): ")
+
+    try:
+        archivos_en_carpeta = os.listdir(ruta)
+        
+        patron = r'^.*\.txt$'
+        lista_txt = []
+    
+        for nombre in archivos_en_carpeta:
+            if re.match(patron, nombre, re.IGNORECASE):
+                lista_txt.append(nombre)
+       
+        if lista_txt:
+            print("\nArchivos encontrados (Arreglo):")
+            print(lista_txt) 
+        else:
+            print("\nNo se encontró ninguna ocurrencia de archivos .txt.")
+
+    except FileNotFoundError:
+        print("Error: La ruta no existe. Verifica que esté bien escrita.")
+    except Exception as e:
+        print(f"Ocurrió un error inesperado: {e}")
+
+
+c()
